d44f1d133ceef1f259ca83b9dc3b2b5a35b78390
Steven Guan <sxg8999@gmail.com>
2020-05-15 14:40:21 -0400

    Added a function that does S-Box affine mapping  in AES

---
+++ Mode/AES.py
@@ -0,0 +1,42 @@
+#This class holds functions related to AES Encryption and Decryption
+#
+#@author Steven Guan
+
+
+def s_box_affine_mapping(inverseVal):
+    """
+    Precondition: inverseVal is a string representation of the Inverse of the 
+                  initial starting hex value. inverseVal is
+                
+    """
+
+    _hexVal = int(inverseVal,16)  #converts inverseVal to hex
+    _binVal = bin(_hexVal)        #converst the hexVal to binary
+    _bin_str = str(_binVal)[2:]
+    _bin_str = _bin_str[::-1]     #reverse the string
+
+
+    # b = matrix_a * b'+ bit_vector mod 2
+    matrix_a = [
+        [1,0,0,0,1,1,1,1],
+        [1,1,0,0,0,1,1,1],
+        [1,1,1,0,0,0,1,1],
+        [1,1,1,1,0,0,0,1],
+        [1,1,1,1,1,0,0,0],
+        [0,1,1,1,1,1,0,0],
+        [0,0,1,1,1,1,1,0],
+        [0,0,0,1,1,1,1,1]
+    ]
+
+    bit_vector = [1,1,0,0,0,1,1,0]
+
+    
+
+
+
+def getCommands():
+    commands = {
+        
+    }
+
+
