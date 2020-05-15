91f6414bc6261c4eb1b27a4d22764cd17d1d1819
Steven Guan <sxg8999@gmail.com>
2020-05-15 00:08:49 -0400

    Created a class named ToolKit which will handle and process user inputs

--- program.py
+++ program.py
@@ -4,6 +4,23 @@
 
 
 
+MODE_DICT ={
+    "AES":AES,
+    "REG": Reg
+}
+
+END_SET = set(["q", "exit", "quit"])
+
+
+class ToolKit():
+    def __init__(self):
+        self.__M0DE_DICT = {
+            "AES": AES,
+            "REG": Reg
+        }
+        self.__END_SET = set(["q", "exit", "quit"])
+    
+
 
 def main():
     pass
