1861d88848d197cd4276e57bf820875e2e16ae38
Steven Guan <sxg8999@gmail.com>
2020-05-15 20:12:08 -0400

    Modified the S-Box affine mapping function to allow both encryption and decryption

--- Module/AES/S_Box.py
+++ Module/AES/S_Box.py
@@ -1,26 +1,21 @@
 import numpy as np
 
-def s_box_affine_mapping(inverseVal):
+def BinStr(_str):
+    _hexVal = int(_str,16)  #converts inverseVal to hex
+    _binVal = bin(_hexVal)        #converst the hexVal to binary
+    _bin_str = str(_binVal)[2:]
+    return _bin_str
+
+
+def s_box_affine_mapping(_val, config = "ENCRYPTION"):
         """
-        Precondition: inverseVal is a string representation of the Inverse of the 
-                    initial starting hex value. inverseVal is
+        Preconditions: 
+            -_val is a string representation of a hexadecimal value
+            - if config = ENCRYPTION then _val must be the inverse of the original hexadecimal value
                     
         """
-
-        _hexVal = int(inverseVal,16)  #converts inverseVal to hex
-        _binVal = bin(_hexVal)        #converst the hexVal to binary
-        _bin_str = str(_binVal)[2:]
-        # print(_bin_str)
-        _bin_str = _bin_str[::-1]     #reverse the string
-
-        b_initial = np.array(list(_bin_str.strip()), dtype = int)
-        b_initial = np.pad(b_initial,(0, 8 -_bin_str.__len__()))
-        print("b_initial", b_initial)
-        b_reshaped = b_initial.reshape((8,1))
-
-        
-        # b = matrix_a * b'+ bit_vector mod 2
-        matrix_a = np.array([
+        matrix_dict = {
+            "ENCRYPTION" : [
             [1,0,0,0,1,1,1,1],
             [1,1,0,0,0,1,1,1],
             [1,1,1,0,0,0,1,1],
@@ -29,13 +24,42 @@ def s_box_affine_mapping(inverseVal):
             [0,1,1,1,1,1,0,0],
             [0,0,1,1,1,1,1,0],
             [0,0,0,1,1,1,1,1]
-        ], dtype = int)
+        ],
+            "DECRYPTION" : [
+            [0,0,1,0,0,1,0,1],
+            [1,0,0,1,0,0,1,0],
+            [0,1,0,0,1,0,0,1],
+            [1,0,1,0,0,1,0,0],
+            [0,1,0,1,0,0,1,0],
+            [0,0,1,0,1,0,0,1],
+            [1,0,0,1,0,1,0,0],
+            [0,1,0,0,1,0,1,0]
+        ]
+        }
 
-        bit_vector = np.array([1,1,0,0,0,1,1,0], dtype = int)
+        bit_vector_dict ={
+            "ENCRYPTION":[1,1,0,0,0,1,1,0],
+            "DECRYPTION":[1,0,1,0,0,0,0,0]
+        }
+
+        matrix = np.array(matrix_dict.get(config), dtype = int)
+        bit_vector = np.array(bit_vector_dict.get(config), dtype = int)
+
+
+        _hexVal = int(_val,16)  #converts inverseVal to hex
+        _binVal = bin(_hexVal)        #converst the hexVal to binary
+        _bin_str = str(_binVal)[2:]
+        _bin_str = _bin_str[::-1]     #reverse the string
+
+        b_initial = np.array(list(_bin_str.strip()), dtype = int)
+        b_initial = np.pad(b_initial,(0, 8 -_bin_str.__len__()))
         
-        multresult = matrix_a.dot(b_reshaped)
+        b_reshaped = b_initial.reshape((8,1))
+
+        
+        multresult = matrix.dot(b_reshaped)
         multresult = multresult.reshape((1,8))
-        print("mult result", multresult)
+    
         
         b_final = multresult.__add__(bit_vector).ravel()
         
@@ -51,11 +75,8 @@ def s_box_affine_mapping(inverseVal):
         print(result)
         
 
-
-
-
-
         
         
 
-s_box_affine_mapping("2f")
+s_box_affine_mapping("2f")                          #OUTPUT: 0x25
+s_box_affine_mapping("25", config = "DECRYPTION")   #OUTPUT: 0x2F
