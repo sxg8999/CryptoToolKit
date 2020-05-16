c76afe8795a0645855d3ddf4c1167f175c6b224f
Steven Guan <sxg8999@gmail.com>
2020-05-15 20:26:27 -0400

    Created a new class named Reg which will hold all the functions related to general cryptography

--- Mode/Reg.py
+++ Mode/Reg.py
@@ -1,2 +1,15 @@
+#This class holds functions related to general cryptography
+#
+#@author Steven Guan
 
+import numpy as np 
 
+class Reg():
+    
+    def __init__(self):
+        self.__command = {
+
+        }
+
+    def getCommands(self):
+        return self.__command
